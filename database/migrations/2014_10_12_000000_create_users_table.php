d8613ae587a36b2bc332c6894cf2fcfcc69a6c9f
allan <moranteallan@gmail.com>
2017-12-15 10:41:53 +0100

    update migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->increments('idMembre');
+            $table->string('nom', 20);
+            $table->string('prenom', 20);
+            $table->string('email', 100)->unique();
+            $table->integer('telephone')->unique();
             $table->string('password');
+            $table->boolean('isAdmin')->default(false);
+            $table->integer('rang')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
