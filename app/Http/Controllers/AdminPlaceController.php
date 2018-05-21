5ae0012cec9fe6725f871e6b5d182756d72a8442
allan <moranteallan@gmail.com>
2018-05-21 20:42:56 +0200

    update laravel and create checkadmin middleware

--- app/Http/Controllers/AdminPlaceController.php
+++ app/Http/Controllers/AdminPlaceController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 class AdminPlaceController extends Controller
 {
+    public function
     /**
      * Display a listing of the resource.
      *
--- app/Http/Middleware/CheckAdmin.php
+++ app/Http/Middleware/CheckAdmin.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class CheckAdmin
 {
@@ -15,6 +16,9 @@ class CheckAdmin
      */
     public function handle($request, Closure $next)
     {
+        if (Auth::) {
+            return redirect('home');
+        }
         return $next($request);
     }
 }
